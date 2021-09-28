e97749c31f1f7b6a987afbfa235e0ca262ca39d9
BeaBea <pcreis.beatriz@gmail.com>
2021-09-28 17:09:40 -0300

    Criação da activity de adição de novos cartões

---
+++ app/src/main/java/bootcamp/inter/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,12 @@
+package bootcamp.inter.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bootcamp.inter.businesscard.R
+
+class AddBusinessCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_business_card)
+    }
+}
