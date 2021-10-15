56dc2a063b276e63470c64b7c32682c58d055f13
BeaBea <pcreis.beatriz@gmail.com>
2021-10-15 10:33:45 -0300

    Classe que instancia o Room e o Repository

---
+++ app/src/main/java/bootcamp/inter/businesscard/App.kt
@@ -0,0 +1,5 @@
+package bootcamp.inter.businesscard
+
+class App {
+    
+}
--- app/src/main/java/bootcamp/inter/businesscard/presenter/MainActivity.kt
+++ app/src/main/java/bootcamp/inter/businesscard/presenter/MainActivity.kt
@@ -1,9 +1,15 @@
-package bootcamp.inter.businesscard.ui
+package bootcamp.inter.businesscard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import bootcamp.inter.businesscard.R
 import bootcamp.inter.businesscard.databinding.ActivityMainBinding
+
+/*
+Essa classe é responsável por inflar o layout activity_main, o qual contém a recycle view com os
+cartões, bem como o campo para pesquisa e o fab para adicionar novos cartões. Ou seja, a tela
+primária do App.
+ */
+
 
 class MainActivity : AppCompatActivity() {
 
